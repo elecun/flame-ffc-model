9c7e1f01fe38043d02b0c686f70ad87f87754256
elecun <elecun2@gmail.com>
2024-12-17 23:22:12 +0900

    fixed

--- v&v/multi-variable_nn.py
+++ v&v/multi-variable_nn.py
@@ -12,14 +12,11 @@ from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 import seaborn as sns
-#from keras.utils.vis_utils import plot_model
 from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error
-# Modeling
-from tensorflow.keras import layers, models
-from tensorflow.keras.optimizers import RMSprop
 
 # 1. Load Raw Data
-csv_raw = pd.read_csv("./data/data.csv", skiprows=1, parse_dates=['datetime'], dayfirst=False)
+csv_raw = pd.read_csv("./data_2024.csv", skiprows=1, parse_dates=['datetime'], dayfirst=False)
+#csv_raw = pd.read_csv("./data/data.csv", skiprows=1, parse_dates=['datetime'], dayfirst=False)
 
 # 2. extract usable raw component(columns) ordered by datatime
 raw_data = csv_raw[['datetime', 'warmup', 'off', 
@@ -27,7 +24,7 @@ raw_data = csv_raw[['datetime', 'warmup', 'off',
                    'velocity','tension','v','a','temperature',
                    'copper_sulfate_content', 'sulfuric_acid_content', 'chloride_content']].sort_values("datetime")
 
-# 3. extracted column data
+# 3. extracted column data, remove NaN
 raw_warmup = raw_data[~raw_data["warmup"].isna()][["datetime", "warmup"]]
 raw_off = raw_data[~raw_data["off"].isna()][["datetime", "off"]]
 raw_brightener = (raw_data[~raw_data["add_amount_brightener"].isna()])[["datetime", "add_amount_brightener"]]
@@ -56,7 +53,6 @@ raw_ext = pd.DataFrame(dtype=float, columns=['datetime', 'optime', 'optime_min',
                                              'cs','cs_var', 'cs_std',
                                              'sa','sa_var', 'sa_std',
                                              'ch','c_var', 'c_std'])
-# print(raw_ext.shape) # 0,29
 
 #5. Calculate operation time (every single day)
 for warmup, off in zip(raw_warmup.iterrows(), raw_off.iterrows()):
@@ -70,18 +66,10 @@ raw_ext = raw_ext[raw_ext["optime"]>datetime.timedelta(seconds=0)] # drop no tim
 raw_ext["optime_min"] = [i.total_seconds()/60 for i in raw_ext.loc[:,"optime"]] # add time min # 작업시간을 분으로 재표현
 raw_ext = raw_ext.drop(columns="optime") # optime 컬럼 제거
 
-print(raw_ext)
+
 
 for t_idx in raw_ext.index: # 중복없는 datetime index를 기준으로 해당 날짜에 대한 데이터 추출
-    
     mask_brightener = list((raw_brightener["datetime"]>t_idx) & (raw_brightener["datetime"]<=t_idx+datetime.timedelta(hours=24)))
-
-    print("---")
-    print(raw_brightener["datetime"])
-    print("start : ", t_idx)
-    print("end : ",t_idx+datetime.timedelta(hours=24))
-    print("---")
-    
     mask_carrier = list((raw_carrier["datetime"]>t_idx) & (raw_carrier["datetime"]<=t_idx+datetime.timedelta(hours=24)))
     mask_voltage = list((raw_voltage["datetime"]>t_idx) & (raw_voltage["datetime"]<=t_idx+datetime.timedelta(hours=24)))
     mask_ampere = list((raw_ampere["datetime"]>t_idx) & (raw_ampere["datetime"]<=t_idx+datetime.timedelta(hours=24)))
@@ -91,49 +79,44 @@ for t_idx in raw_ext.index: # 중복없는 datetime index를 기준으로 해당
     mask_cs = list((raw_cs["datetime"]>t_idx) & (raw_cs["datetime"]<=t_idx+datetime.timedelta(hours=24)))
     mask_sa = list((raw_sa["datetime"]>t_idx) & (raw_sa["datetime"]<=t_idx+datetime.timedelta(hours=24)))
     mask_c = list((raw_c["datetime"]>t_idx) & (raw_c["datetime"]<=t_idx+datetime.timedelta(hours=24)))
-    
-    
-    print(raw_ext.loc[:, "add_amount_brightener"])
-    print(mask_brightener)
-    
+
     
     if any(mask_brightener):
-        raw_ext.loc[t_idx, "add_amount_brightener"] = raw_brightener.loc[mask_brightener, "add_amount_brightener"].values[0]
+        raw_ext.loc[t_idx, "add_amount_brightener"] = raw_brightener.loc[mask_brightener, "add_amount_brightener"].astype(float).values[0]
     if any(mask_carrier):
-        raw_ext.loc[t_idx, "add_amount_carrier"] = raw_carrier.loc[mask_carrier, "add_amount_carrier"].values[0]
+        raw_ext.loc[t_idx, "add_amount_carrier"] = raw_carrier.loc[mask_carrier, "add_amount_carrier"].astype(float).values[0]
     if any(mask_voltage):
-        raw_ext.loc[t_idx, "voltage"] = raw_voltage.loc[mask_voltage, "v"].values.mean()
-        raw_ext.loc[t_idx, "voltage_var"] = raw_voltage.loc[mask_voltage, "v"].values.var()
-        raw_ext.loc[t_idx, "voltage_std"] = raw_voltage.loc[mask_voltage, "v"].values.std()
+        raw_ext.loc[t_idx, "voltage"] = raw_voltage.loc[mask_voltage, "v"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "voltage_var"] = raw_voltage.loc[mask_voltage, "v"].astype(float).values.var()
+        raw_ext.loc[t_idx, "voltage_std"] = raw_voltage.loc[mask_voltage, "v"].astype(float).values.std()
     if any(mask_ampere):
-        raw_ext.loc[t_idx, "ampere"] = raw_ampere.loc[mask_voltage, "a"].values.mean()
-        raw_ext.loc[t_idx, "ampere_var"] = raw_ampere.loc[mask_voltage, "a"].values.var()
-        raw_ext.loc[t_idx, "ampere_std"] = raw_ampere.loc[mask_voltage, "a"].values.std()
+        raw_ext.loc[t_idx, "ampere"] = raw_ampere.loc[mask_ampere, "a"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "ampere_var"] = raw_ampere.loc[mask_ampere, "a"].astype(float).values.var()
+        raw_ext.loc[t_idx, "ampere_std"] = raw_ampere.loc[mask_ampere, "a"].astype(float).values.std()
     if any(mask_temperature):
-        #print(raw_temperature.loc[mask_temperature, "temperature"].values)
-        raw_ext.loc[t_idx, "temperature"] = raw_temperature.loc[mask_temperature, "temperature"].values.mean()
-        raw_ext.loc[t_idx, "temperature_var"] = raw_temperature.loc[mask_temperature, "temperature"].values.var()
-        raw_ext.loc[t_idx, "temperature_std"] = raw_temperature.loc[mask_temperature, "temperature"].values.std()
+        raw_ext.loc[t_idx, "temperature"] = raw_temperature.loc[mask_temperature, "temperature"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "temperature_var"] = raw_temperature.loc[mask_temperature, "temperature"].astype(float).values.var()
+        raw_ext.loc[t_idx, "temperature_std"] = raw_temperature.loc[mask_temperature, "temperature"].astype(float).values.std()
     if any(mask_cs):
-        raw_ext.loc[t_idx, "cs"] = raw_cs.loc[mask_cs, "copper_sulfate_content"].values.mean()
-        raw_ext.loc[t_idx, "cs_var"] = raw_cs.loc[mask_cs, "copper_sulfate_content"].values.var()
-        raw_ext.loc[t_idx, "cs_std"] = raw_cs.loc[mask_cs, "copper_sulfate_content"].values.std()
+        raw_ext.loc[t_idx, "cs"] = raw_cs.loc[mask_cs, "copper_sulfate_content"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "cs_var"] = raw_cs.loc[mask_cs, "copper_sulfate_content"].astype(float).values.var()
+        raw_ext.loc[t_idx, "cs_std"] = raw_cs.loc[mask_cs, "copper_sulfate_content"].astype(float).values.std()
     if any(mask_sa):
-        raw_ext.loc[t_idx, "sa"] = raw_sa.loc[mask_sa, "sulfuric_acid_content"].values.mean()
-        raw_ext.loc[t_idx, "sa_var"] = raw_sa.loc[mask_sa, "sulfuric_acid_content"].values.var()
-        raw_ext.loc[t_idx, "sa_std"] = raw_sa.loc[mask_sa, "sulfuric_acid_content"].values.std()
+        raw_ext.loc[t_idx, "sa"] = raw_sa.loc[mask_sa, "sulfuric_acid_content"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "sa_var"] = raw_sa.loc[mask_sa, "sulfuric_acid_content"].astype(float).values.var()
+        raw_ext.loc[t_idx, "sa_std"] = raw_sa.loc[mask_sa, "sulfuric_acid_content"].astype(float).values.std()
     if any(mask_c):
-        raw_ext.loc[t_idx, "ch"] = raw_c.loc[mask_cs, "chloride_content"].values.mean()
-        raw_ext.loc[t_idx, "ch_var"] = raw_c.loc[mask_cs, "chloride_content"].values.var()
-        raw_ext.loc[t_idx, "ch_std"] = raw_c.loc[mask_cs, "chloride_content"].values.std()
+        raw_ext.loc[t_idx, "ch"] = raw_c.loc[mask_c, "chloride_content"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "ch_var"] = raw_c.loc[mask_c, "chloride_content"].astype(float).values.var()
+        raw_ext.loc[t_idx, "ch_std"] = raw_c.loc[mask_c, "chloride_content"].astype(float).values.std()
     if any(mask_velocity):
-        raw_ext.loc[t_idx, "velocity"] = raw_velocity.loc[mask_velocity, "velocity"].values.mean()
-        raw_ext.loc[t_idx, "velocity_var"] = raw_velocity.loc[mask_velocity, "velocity"].values.var()
-        raw_ext.loc[t_idx, "velocity_std"] = raw_velocity.loc[mask_velocity, "velocity"].values.std()
+        raw_ext.loc[t_idx, "velocity"] = raw_velocity.loc[mask_velocity, "velocity"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "velocity_var"] = raw_velocity.loc[mask_velocity, "velocity"].astype(float).values.var()
+        raw_ext.loc[t_idx, "velocity_std"] = raw_velocity.loc[mask_velocity, "velocity"].astype(float).values.std()
     if any(mask_tension):
-        raw_ext.loc[t_idx, "tension"] = raw_tension.loc[mask_tension, "tension"].values.mean()
-        raw_ext.loc[t_idx, "tension_var"] = raw_tension.loc[mask_tension, "tension"].values.var()
-        raw_ext.loc[t_idx, "tension_std"] = raw_tension.loc[mask_tension, "tension"].values.std()
+        raw_ext.loc[t_idx, "tension"] = raw_tension.loc[mask_tension, "tension"].astype(float).values.mean()
+        raw_ext.loc[t_idx, "tension_var"] = raw_tension.loc[mask_tension, "tension"].astype(float).values.var()
+        raw_ext.loc[t_idx, "tension_std"] = raw_tension.loc[mask_tension, "tension"].astype(float).values.std()
 
 
         
@@ -150,7 +133,7 @@ brightener_scaler = StandardScaler()
 carrier_scaler = StandardScaler()
 voltage_scaler = StandardScaler()
 ampere_scaler = StandardScaler()
-temperaature_scaler = StandardScaler()
+temperature_scaler = StandardScaler()
 cs_scaler = StandardScaler()
 sa_scaler = StandardScaler()
 ch_scaler = StandardScaler()
@@ -162,7 +145,7 @@ add_amount_brightener_normalized = brightener_scaler.fit_transform(raw_prepared.
 add_amount_carrier_normalized = carrier_scaler.fit_transform(raw_prepared.loc[:,"add_amount_carrier"].ravel().reshape(-1, 1)).flatten()
 voltage_normalized = voltage_scaler.fit_transform(raw_prepared.loc[:,"voltage"].ravel().reshape(-1, 1)).flatten()
 ampere_normalized = ampere_scaler.fit_transform(raw_prepared.loc[:,"ampere"].ravel().reshape(-1, 1)).flatten()
-temperature_normalized = temperaature_scaler.fit_transform(raw_prepared.loc[:,"temperature"].ravel().reshape(-1, 1)).flatten()
+temperature_normalized = temperature_scaler.fit_transform(raw_prepared.loc[:,"temperature"].ravel().reshape(-1, 1)).flatten()
 cs_normalized = cs_scaler.fit_transform(raw_prepared.loc[:,"cs"].ravel().reshape(-1, 1)).flatten()
 sa_normalized = sa_scaler.fit_transform(raw_prepared.loc[:,"sa"].ravel().reshape(-1, 1)).flatten()
 velocity_normalized = velocity_scaler.fit_transform(raw_prepared.loc[:,"velocity"].ravel().reshape(-1, 1)).flatten()
@@ -188,7 +171,7 @@ velocity_kde = gaussian_kde(velocity_normalized, bw_method=bw)
 tension_kde = gaussian_kde(tension_normalized, bw_method=bw)
 
 # resample from kde
-n_sample = 1000
+n_sample = 2000
 
 x_optime = np.linspace(min(optime_mean_normalized), max(optime_mean_normalized), n_sample)
 x_brightener = np.linspace(min(add_amount_brightener_normalized), max(add_amount_brightener_normalized), n_sample)
@@ -218,7 +201,7 @@ rx_brightener = brightener_scaler.inverse_transform(x_brightener.reshape(1, -1))
 rx_carrier = carrier_scaler.inverse_transform(x_carrier.reshape(1, -1)).flatten()
 rx_voltage = voltage_scaler.inverse_transform(x_voltage.reshape(1, -1)).flatten()
 rx_ampere = ampere_scaler.inverse_transform(x_ampere.reshape(1, -1)).flatten()
-rx_temperature = temperaature_scaler.inverse_transform(x_temperature.reshape(1, -1)).flatten()
+rx_temperature = temperature_scaler.inverse_transform(x_temperature.reshape(1, -1)).flatten()
 rx_cs = cs_scaler.inverse_transform(x_cs.reshape(1, -1)).flatten()
 rx_sa = sa_scaler.inverse_transform(x_sa.reshape(1, -1)).flatten()
 rx_velocity = velocity_scaler.inverse_transform(x_velocity.reshape(1, -1)).flatten()
@@ -241,16 +224,18 @@ plt.title('Kernel Density Estimation Functions')
 plt.legend()
 plt.show()
 
+# Modeling
+from tensorflow.keras import layers, models
+from tensorflow.keras.optimizers import RMSprop
+
 
 # correlation
 X = pd.DataFrame([x_optime.T, x_voltage.T, x_ampere.T, x_temperature.T, x_cs.T, x_sa.T, x_tension.T, x_velocity.T]).T
 y = pd.DataFrame([x_brightener.T, x_carrier.T]).T
 X.columns = ['optime', 'voltage', 'Ampere', "Temperature", "copper_sulfate", "sulfuric_acid", "Tension", "Velocity"]
 y.columns = ['brightener', 'carrier']
-# corr = X.corr()
-# sns.heatmap(corr, cmap='viridis')
-# plt.show()
 
+# print(X)
 
 # model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
@@ -261,11 +246,14 @@ model.add(layers.Dense(5, activation='relu'))
 model.add(layers.Dense(y.shape[1], activation='linear'))
 
 
-#plot_model(model, to_file="./model.png", show_shapes=True, show_layer_names=True)
 model.compile(loss='mean_squared_error', optimizer='sgd')
 print(f"X_train:{X_train.shape}, y_train:{y_train.shape}, X_test:{X_test.shape}, y_test:{y_test.shape}")
 history = model.fit(X_train, y_train, epochs=20, batch_size=5, verbose=1, validation_data=(X_test, y_test))
 y_pred = model.predict(X_test)
+
+# save model
+model.save("./mvnn.keras")
+model.summary()
 
 # output inverse transform
 brightener_pred_output = brightener_scaler.inverse_transform(y_pred[:,0].reshape(1, -1)).flatten()
@@ -276,7 +264,7 @@ carrier_test_output = carrier_scaler.inverse_transform(y_test["carrier"].values.
 plt.scatter(brightener_test_output, brightener_pred_output, color='b', label='Brightener')
 plt.xlabel('Actual')
 plt.ylabel('Predicted')
-plt.title('Multivariate Neural Network Prediction Results of Brighener')
+plt.title('Multivariate Neural Network Prediction Results of Brightener')
 plt.legend()
 plt.show()
 
@@ -297,14 +285,17 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-mae = mean_absolute_error(carrier_test_output, carrier_pred_output)
-print(f"Mean Absolute Error : {mae}")
 
-mse = mean_squared_error(carrier_test_output, carrier_pred_output)
-print(f"Mean Squared Error : {mse}")
-
-r2 = r2_score(y_test, y_pred)
-print(f"R-squared Result : {r2}")
-
-vs = explained_variance_score(carrier_test_output, carrier_pred_output)
-print(f"Explained Variance Score : {vs}")
+carrier_mae = mean_absolute_error(carrier_test_output, carrier_pred_output)
+print(f"MAE of Carrier : {carrier_mae}")
+carrier_mse = mean_squared_error(carrier_test_output, carrier_pred_output)
+print(f"MSE of Carrier : {carrier_mse}")
+carrier_r2 = r2_score(carrier_test_output, carrier_pred_output)
+print(f"R-squared of Carrier : {carrier_r2}")
+print("-------------------------------")
+brightener_mae = mean_absolute_error(brightener_test_output, brightener_pred_output)
+print(f"MAE of Brightener : {brightener_mae}")
+brightener_mse = mean_squared_error(brightener_test_output, brightener_pred_output)
+print(f"MSE of Brightener : {brightener_mse}")
+brightener_r2 = r2_score(brightener_test_output, brightener_pred_output)
+print(f"R-squared of Brightener : {brightener_r2}")
